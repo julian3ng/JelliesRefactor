e0ecfce7f04d989b65cd0ee522d4976a643ad1cd
Julian Eng <julian3ng@gmail.com>
2016-10-08 13:30:55 -0400

    Update map constructor and creation

--- include/map.h
+++ include/map.h
@@ -1,8 +1,12 @@
 #ifndef MAP_H
 #define MAP_H
 
-int map_create(tile ***m);
-int map_destroy(tile **m);
+char tile_glyph(tile_t *t);
+tile_type tile_get_type(tile_t *t);
+tile_t *map_tile_at(int y, int x, map_t m);
+map_t map_create(void);
+int map_destroy(map_t m);
+int map_init(map_t *m);
 
 
 #endif /* MAP_H */
--- src/models/map.c
+++ src/models/map.c
@@ -1,25 +1,56 @@
 #include "rogue.h"
 #include <stdlib.h>
 
-int map_create(tile ***m) {
-    *m = (tile **) malloc(sizeof(tile *) * MAP_HEIGHT);
 
+
+typedef enum tile_flags {
+    UNFLAGGED = LSHIFT(0),
+    FUTURE_WALL = LSHIFT(1),
+    FUTURE_FLOOR = LSHIFT(2),
+} tile_flags;
+
+typedef struct tile {
+    tile_type type;
+    tile_flags flags;
+    char glyph;
+    creature_t *c;
+} tile;
+
+
+tile_type tile_get_type(tile_t *t) {
+    return t->type;
+}
+char tile_get_glyph(tile_t *t) {
+    return t->glyph;
+}
+
+map_t map_create() {
+    tile **m = (tile **) malloc(sizeof(tile *) * MAP_HEIGHT);
     for (int i=0; i<MAP_HEIGHT; i++) {
-        (*m)[i] = (tile *) malloc(sizeof(tile) * MAP_WIDTH);
+        m[i] = (tile *) malloc(sizeof(tile) * MAP_WIDTH);
     }
 
+    return m;
+}
+
+int map_init(map_t *m) {
     for (int i=0; i<MAP_HEIGHT; i++) {
         for (int j=0; j<MAP_WIDTH; j++) {
-            (*m)[i][j] = (tile) {FLOOR, UNFLAGGED, '.', NULL};
+            if ((i == 0) || (i == MAP_HEIGHT-1) || (j == 0) || (j == MAP_WIDTH-1)) {
+                (*m)[i][j] = (tile) {FLOOR, UNFLAGGED, '#', NULL};
+            } else {
+                (*m)[i][j] = (tile) {FLOOR, UNFLAGGED, '.', NULL};
+            }
         }
     }
+
     return 0;
 }
 
-int map_init(tile ***m) {
+tile_t *map_tile_at(int y, int x, map_t m) {
+    return &m[y][x];
 }
-
-int map_destroy(tile **m) {
+int map_destroy(map_t m) {
     for (int i=0; i<MAP_HEIGHT; i++) {
         free(m[i]);
     }
